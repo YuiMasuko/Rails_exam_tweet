fca4ee77a7abded001fdd1bfa998e9d4e004b4e7
yui masuko <yui.t.tom@gmail.com>
2021-07-24 10:02:06 +0900

    一覧画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  def create
+      Post.create(post_params)
+      redirect_to new_post_path
+  end
+  def index
+    @posts = Post.all
+  end
+
+
+  def post_params
+      params.require(:post).permit(:content)
+  end
 end
