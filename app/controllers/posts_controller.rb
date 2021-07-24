b3ceb9f9db3b8de406a3cb0571b67015c359128b
yui masuko <yui.t.tom@gmail.com>
2021-07-24 10:53:42 +0900

    編集 削除機能 共通化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
   def new
     @post = Post.new
   end
@@ -13,9 +14,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def edit
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました！"
+  end
 
-
+private
   def post_params
       params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
