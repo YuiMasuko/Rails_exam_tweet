26221d5286d6512df7e75b4fd70a94ad1cd57e15
yui masuko <yui.t.tom@gmail.com>
2021-07-24 12:31:46 +0900

    投稿確認画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,27 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
   def new
     @post = Post.new
   end
   def create
       @post = Post.new(post_params)
+      if params[:back]
+        render :new
+      else
       if @post.save
-        redirect_to new_post_path, notice: "投稿しました！"
+        redirect_to posts_path, notice: "投稿しました！"
       else
         render :new
       end
   end
-  def index
-    @posts = Post.all
-  end
   def edit
   end
-  def update
+  def show
+  end
+    def update
     if @post.update(post_params)
       redirect_to posts_path, notice: "編集しました！"
     else
@@ -27,12 +32,16 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to posts_path, notice:"削除しました！"
   end
-
-private
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+  def set_post
+      @post = Post.find(params[:id])
+  end
+  private
   def post_params
       params.require(:post).permit(:content)
   end
-  def set_post
-    @post = Post.find(params[:id])
   end
 end
